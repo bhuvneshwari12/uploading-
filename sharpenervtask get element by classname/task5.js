95c3dda93ee01596bf07968fc5083ae6088edeab
bhuvneshwari12 <148437144+bhuvneshwari12@users.noreply.github.com>
2023-10-24 16:53:31 +0530

    sharpener task 5 get element by classname

---
+++ sharpenervtask get element by classname/task5.js
@@ -0,0 +1,9 @@
+//editing the new li using class name //
+
+var item_1=document.getElementsByClassName("newitem");
+item_1[0].style.color='brown'
+
+//editing the new li using tagname//
+
+var item_2=document.getElementsByTagName('li');
+item_2[4].style.backgroundColor='yellow'
